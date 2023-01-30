64a3e29f4473a3618679df914dcaa55ea6d69114
Baxtiyar Mammadyarov <74107721+BaxtiyarMammadyarov@users.noreply.github.com>
2023-01-31 00:25:45 +0400

    pobje

--- to_do_list/index.js
+++ to_do_list/index.js
@@ -1,29 +1,45 @@
 const btn_div = document.querySelector(".btn-div");
 const sorted_btn = document.querySelector(".sorted-btn");
 const v_ul = document.querySelector('.list-ul');
-const input = document.querySelector(".input input");;
-const v_list = [];
+const input = document.querySelector(".input input");
+let v_list = [];
+let id = 1;
 let sorted = true;
+
 
 function appendList(list) {
     let v_add = '';
 
     v_list.forEach(v => {
         //<li><span>tesr1</span> <span class="sil"><img src="img/sil.svg" alt=""></span></li>
-        v_add += `<li><span>${v}</span> <span class="sil"><img src="img/sil.svg" alt=""></span></li>`
+        v_add += `<li class ='row' ><span>${v.name}</span> <button class="sil" onclick = deleteRow(${v.id})><img src="img/sil.svg" alt=""></button></li>`
 
     })
     return v_add;
 }
 
+function deleteRow(row){
+  
+   v_list = v_list.filter(v => {return v.id !==row});
+   
+    v_ul.innerHTML = appendList(v_list);
+    
+  }
+
 btn_div.addEventListener('click', function () {
 
     if (input.value.trim().length !== 0) {
-        v_list.unshift(input.value);
+
+        let obj = {
+            "id": id,
+            "name": input.value
+        }
+        id++
+        v_list.unshift(obj);
         input.value = '';
         v_ul.innerHtml = '';
         // v_list.reverse();
-        console.log(v_list)
+    
 
         v_ul.innerHTML = appendList(v_list);
     }
@@ -34,16 +50,18 @@ sorted_btn.addEventListener('click', function () {
     let v_add = '';
     if (sorted) {
         v_list.sort(function (m, n) {
-            return n.localeCompare(m);
+            return n.name.localeCompare(m.name);
         });
         sorted = false;
     }
     else {
         v_list.sort(function (m, n) {
-            return m.localeCompare(n);
+            return m.name.localeCompare(n.name);
         });
         sorted = true;
     }
     v_ul.innerHTML = appendList(v_list);
 
 });
+
+
